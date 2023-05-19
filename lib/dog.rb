275f93114f7cfc4d98361a4d42722e9a835a65eb
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 07:45:30 +0300

    updates

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
-
+    def initialize(name, breed)
+        @name = name
+        @breed = breed
+      end
+      
+      # Getter method for @name
+      def name
+        @name
+      end
+      
+      # Setter method for @name
+      def name=(new_name)
+        @name = new_name
+      end
+      
+      # Getter method for @breed
+      def breed
+        @breed
+      end
+      
+      # Setter method for @breed
+      def breed=(new_breed)
+        @breed = new_breed
+      end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
+    def name
+      @name
+    end
+  
+    def name=(new_name)
+      @name = new_name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(new_job)
+      @job = new_job
+    end
+  end
+  
 
-end
+  person = Person.new
+person.name = "John Doe"
+person.job = "Engineer"
+
+puts person.name
+# Output: John Doe
+
+puts person.job
+# Output: Engineer
