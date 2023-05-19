31c8a694cec7281227d04ea309c4e33fe183f0cb
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 07:48:39 +0300

    updates

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,3 +24,14 @@ class Dog
         @breed = new_breed
       end
 end
+
+dog = Dog.new("Buddy", "Labrador")
+puts dog.name  # Output: Buddy
+puts dog.breed # Output: Labrador
+
+dog.name = "Max"
+dog.breed = "Golden Retriever"
+
+puts dog.name  # Output: Max
+puts dog.breed # Output: Golden Retriever
+
